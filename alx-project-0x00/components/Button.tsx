f1cce945e7631df65913d8f2aa216a31f3bc36bc
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-05-27 16:04:10 +0300

    Advanced Task v3

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,6 +1,6 @@
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, className }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button className={`border px-3 py-1 text-lg mt-3 bg-purple-700 text-white ${styles}`}>
       {title}
